b146b6f6e4ab3d9928a30915cdf25accb8397bd9
ezra31448 <ezrapiya.th@gmail.com>
2022-04-28 11:14:19 +0700

    edit receive data from user (editcomponent.js)

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const blogRoute = require('./route/blog');
+const authRoute = require('./route/auth');
+
+const app = express();
+
+// connect cloud database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser:true,
+    useUnifiedTopology:false
+}).then(()=> console.log(`Cloud server status: >>Connected!!<<`))
+.catch((err)=>console.log(`Error Can't connect to server`))
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//route
+app.use('/api', blogRoute);
+app.use('/api', authRoute);
+
+//Port confiq
+const port = process.env.PORT || 8080
+app.listen(port, ()=>console.log(`Start server in port ${port}`));
+
