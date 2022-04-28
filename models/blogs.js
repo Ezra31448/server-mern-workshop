d9e809261cf4f641421e8fba15d5673a09a39b02
ezra31448 <ezrapiya.th@gmail.com>
2022-04-28 11:04:58 +0700

    edit receive data from user (editcomponent.js)

---
+++ models/blogs.js
@@ -0,0 +1,32 @@
+//Title, Content, Author, Slug(url)
+//TimeStamp
+// Model for Data Structure
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+    title:{
+        type:String, 
+        required:true
+    },
+    content:{
+        type:{},
+        required:true
+    },
+    author:{
+        type:String,
+        default:"Admin"
+    },
+    slug:{
+        type:String,
+        lowercase:true,
+        unique:true
+    },
+    type: "",
+    duration: "",
+    date: "",
+    
+}
+, {timestamps:true}
+);
+
+module.exports = mongoose.model("Blogs", blogSchema);
